351fc400f831fa57a0856badffec9da576c4f946
Yerassyl Kenes <68700872+warptengood@users.noreply.github.com>
2026-07-02 20:57:29 +0900

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            if self.stack[-1][1] < val:
+                self.stack.append((val, self.stack[-1][1]))
+            else:
+                self.stack.append((val, val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
